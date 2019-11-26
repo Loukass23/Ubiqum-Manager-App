ddb4a2e1c2d08a9a23a449cd053863dfba69a0e6
loukass23 <l.dupias@net.estia.fr>
2019-11-26 16:51:06 +0100

    MUI Table component

---
+++ src/components/TableRender.tsx
@@ -0,0 +1,95 @@
+import MUIDataTable, { MUIDataTableColumnDef, MUIDataTableColumn, MUIDataTableColumnOptions, MUIDataTableOptions } from "mui-datatables";
+import React from 'react';
+
+import { Container, Theme, createStyles, withStyles } from "@material-ui/core";
+
+const styles = (theme: Theme) => createStyles({
+    root: {
+        width: '100%',
+        marginTop: theme.spacing(3),
+    },
+    toolbar: theme.mixins.toolbar,
+    paper: {
+        color: theme.palette.secondary.light
+    }
+
+
+})
+const extractColumns = (data: Array<Object>) => {
+    const dataItem = data[0]
+
+    const keys = Object.keys(dataItem) as (keyof Object)[]
+    const columns: MUIDataTableColumn[] = keys.map((key) => {
+        console.log(typeof (dataItem[key]));
+        if (typeof (dataItem[key]) !== 'object') {
+
+            const upper = key.replace(/^\w/, c => c.toUpperCase());
+            const label = upper.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+            const col: MUIDataTableColumn = {
+                name: key,
+                label: label,
+                options: {
+                    filter: true,
+                    sort: true,
+                }
+            }
+            return col
+        }
+        else {
+            const col: MUIDataTableColumn = {
+                name: '',
+                label: '',
+                options: {
+                    filter: true,
+                    sort: true,
+                }
+            }
+            return col
+        }
+    })
+        .filter(key => key.name !== '')
+    return columns
+}
+
+const getOptions = (title: string) => {
+
+    return {
+        // filterType: 'dropdown',
+        pagination: false,
+        downloadOptions: {
+            filename: `${title}.csv`,
+            separator: ',',
+            filterOptions: {
+                useDisplayedColumnsOnly: true,
+                useDisplayedRowsOnly: true
+            }
+
+        },
+    };
+}
+
+interface Props {
+    data: Array<Object>,
+    classes: any,
+    title: string
+
+}
+const TableRender: React.FC<Props> = ({ data, classes, title }) => {
+    const option: MUIDataTableOptions = getOptions(title)
+    const columns: MUIDataTableColumnDef[] = extractColumns(data)
+
+    return (
+        <React.Fragment>
+            <div className={classes.toolbar} />
+            <MUIDataTable
+                title={title}
+                data={data}
+                columns={columns}
+                options={option}
+            />
+        </React.Fragment>
+    )
+}
+
+export default withStyles(styles)(TableRender)
